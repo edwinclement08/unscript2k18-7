b2b81cc46e9fa43fdded597ba3d88a6cd0253d84
Joshua Noronha <joshua.noronha13@gmail.com>
2018-03-24 23:41:47 +0530

    student-application completed

--- backend/frontend/src/app/company-page/company-page.component.ts
+++ backend/frontend/src/app/company-page/company-page.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class CompanyPageComponent implements OnInit {
   company:any = {
     name : "Morgan Stanley",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    description: "Morgan Stanley mobilizes capital to help governments, corporations, institutions and individuals around the world achieve their financial goals. For over 75 years, the firm’s reputation for using innovative thinking to solve complex problems has been well earned and rarely matched. A consistent industry leader throughout decades of dramatic change in modern finance, Morgan Stanley will continue to break new ground in advising, serving and providing new opportunities for its clients. Morgan Stanley is committed to maintaining the first-class service and high standard of excellence that have always defined the firm. At its foundation are four core values — putting clients first, doing the right thing, leading with exceptional ideas and giving back — that guide its more than 55,000 employees",
     website:"http://www.morganstanley.com",
     headquarters:"New York, NY",
     company_type:"Public Company",
@@ -17,6 +17,18 @@ export class CompanyPageComponent implements OnInit {
     job_profile:"Java and Javascript Developer"
   }
   flag:string="company"
+  studentList:any = [
+    {
+      name:"Joshua Noronha",
+      resume:"https://www.linkedin.com/company/morgan-stanley/",
+      isSelected:true
+    },
+    {
+      name:"Joshua Noronha",
+      resume:"https://www.linkedin.com/company/morgan-stanley/",
+      isSelected:true
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
--- backend/frontend/src/app/student-applications/student-applications.component.ts
+++ backend/frontend/src/app/student-applications/student-applications.component.ts
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-applications.component.css']
 })
 export class StudentApplicationsComponent implements OnInit {
-
+  studentList = [
+    {
+      name:"Joshua Noronha",
+      resume:"https://www.linkedin.com/company/morgan-stanley/",
+      isSelected:true
+    },
+    {
+      name:"Joshua Noronha",
+      resume:"https://www.linkedin.com/company/morgan-stanley/",
+      isSelected:true
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
