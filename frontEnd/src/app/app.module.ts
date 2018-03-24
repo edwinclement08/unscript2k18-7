d934932ad1bc38f18e476ec478b7e32f758d5fc0
Joshua Noronha <joshua.noronha13@gmail.com>
2018-03-24 17:35:43 +0530

    Landing page generated
    Landing page generated
    Structure of code is done

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LandingStatisticsComponent } from './landing-statistics/landing-statistics.component';
+import { LandingEventComponent } from './landing-event/landing-event.component';
+import { NoticeBoardComponent } from './notice-board/notice-board.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    LandingStatisticsComponent,
+    LandingEventComponent,
+    NoticeBoardComponent
   ],
   imports: [
     BrowserModule
